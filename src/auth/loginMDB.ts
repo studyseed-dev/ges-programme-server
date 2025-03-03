21f15f88dbc3e50e25566d8508c5e25db46b82c5
Jun Yap <junyap0122@gmail.com>
2025-03-03 22:39:21 +0000

    tweak login router error message

--- src/auth/loginMDB.ts
+++ src/auth/loginMDB.ts
@@ -6,13 +6,18 @@ const router = Router();
 router.post("/login", async (req: Request, res: Response) => {
   try {
     const userLogin = await User.findOne({ userid: req.body.userid });
+
     if (!userLogin) {
-      res.status(401).send({ message: "User does not exist!", operation: false });
+      res.status(401).json({ message: "User does not exist!", operation: false });
       return;
     }
-    res.status(200).send({ message: "Login successful!", operation: true });
+    if (!userLogin.enrolled_courses || userLogin.enrolled_courses.length <= 0) {
+      res.status(404).json({ message: "User not registered!", operation: false });
+      return;
+    }
+    res.status(200).json({ message: "Login successful!", operation: true });
   } catch (error) {
-    res.status(500).send({ error, operation: false });
+    res.status(500).json({ error, operation: false });
   }
 });
 
