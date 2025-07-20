913d1154b6b1d271bb9c347c775ccef11255dde8
studyseed-dev <marketing@studyseed.co.uk>
2025-07-20 23:13:44 +0100

    feat: implement cookies in find endpoint (#7)
    Co-authored-by: Jun Yap <junyap0122@gmail.com>

--- src/auth/loginMDB.ts
+++ src/auth/loginMDB.ts
@@ -1,23 +1,51 @@
-import { Request, Response, Router } from "express";
+import { z } from "zod";
+import jwt from "jsonwebtoken";
+import { Router, Request, Response } from "express";
+
 import { User } from "../models";
 
 const router = Router();
 
-router.post("/login", async (req: Request, res: Response) => {
-  try {
-    const userLogin = await User.findOne({ userid: req.body.userid });
+const loginSchema = z.object({
+  userid: z.string().min(2, "UserID is required"),
+});
 
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+router.post("/login", async (req: Request, res: Response) => {
+  // Validate input
+  const parseResult = loginSchema.safeParse(req.body);
+
+  if (!parseResult.success) {
+    res.status(400).json({
+      message: parseResult.error.issues[0]?.message || "Invalid input",
+      operation: false,
+    });
+  }
+  const parsedUserid = parseResult.data;
+
+  try {
+    const userLogin = await User.findOne({ userid: parsedUserid?.userid });
     if (!userLogin) {
       res.status(401).json({ message: "User does not exist!", operation: false });
-      return;
     }
-    if (!userLogin.enrolled_courses || userLogin.enrolled_courses.length <= 0) {
+    if (!userLogin?.enrolled_courses || userLogin.enrolled_courses.length <= 0) {
       res.status(404).json({ message: "User not registered!", operation: false });
-      return;
     }
-    res.status(200).json({ message: "Login successful!", operation: true });
+
+    // Generate JWT token
+    const token = jwt.sign({ userid: userLogin?.userid }, JWT_SECRET, { expiresIn: "7d" });
+    // Set token as HTTP-only cookie
+    res
+      .cookie("authToken", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        path: "/",
+      })
+      .json({ message: "Login successful!", operation: true, headers: res.getHeaders() });
   } catch (error) {
-    res.status(500).json({ error, operation: false });
+    res.status(500).json({ message: `Login error - ${error}`, operation: false });
   }
 });
 
